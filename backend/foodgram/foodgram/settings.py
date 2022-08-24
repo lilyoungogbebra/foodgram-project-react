c80a62a51f538b98b90bdc4f7208cbc437132554
Ян Мелехин <vladmakaroff.makarov@yandex.ru>
2022-08-24 15:58:41 +0500

    dadahdfsd

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kj-gwa9f85m&2*e8x#f-8d@&)&sc^2x4rz)ct6h$21!_*r)hd5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.29.141', '127.0.0.1', 'localhost', 'foodgram-practicum.ddns.net']
 
