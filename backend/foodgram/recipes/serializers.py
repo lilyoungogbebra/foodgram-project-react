464f0aff6caa11ee829bf3c813c0607e54a9f3e0
Ян Мелехин <vladmakaroff.makarov@yandex.ru>
2022-08-28 21:42:47 +0500

    skdkofnjjnjfsf

--- backend/foodgram/recipes/serializers.py
+++ backend/foodgram/recipes/serializers.py
@@ -6,7 +6,6 @@ from rest_framework import serializers
 
 from .models import (Ingredient, Recipe, RecipeIngredientRelationship,
                      RecipeTagRelationship, Tag)
-from .utils import create_relationship_ingredient_recipe
 from users.serializers import UserSerializer
 
 User = get_user_model()
@@ -106,6 +105,29 @@ def create_relationship_tag_recipe(tags, recipe):
     '''Наполнение связующей таблицы тегами и рецептами.'''
     for tag in tags:
         RecipeTagRelationship.objects.create(tag=tag, recipe=recipe)
+
+
+def create_relationship_ingredient_recipe(ingredients, recipe):
+    '''Наполнение связующей таблицы ингредиентами и рецептами.'''
+    ingredient_list = []
+    for ingredient in ingredients:
+        cur_id = ingredient['id']
+        current_ingredient = Ingredient.objects.filter(id=cur_id).first()
+        if not current_ingredient:
+            message = (
+                f'Недопустимый первичный ключ \"{cur_id}\" -'
+                + 'ингредиента не существует в нашей бд.'
+            )
+            raise serializers.ValidationError(
+                {'ingredients': [f'{message}']}
+            )
+        new_ingredient = RecipeIngredientRelationship(
+            recipe=recipe,
+            ingredient_id=cur_id,
+            amount=ingredient['amount'],
+        )
+        ingredient_list.append(new_ingredient)
+    RecipeIngredientRelationship.objects.bulk_create(ingredient_list)
 
 
 class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
