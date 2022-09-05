f5ff91cfbbb074261dd77d269e0cfc45c171527f
Ян Мелехин <vladmakaroff.makarov@yandex.ru>
2022-09-05 21:31:28 +0500

    ffhsasdj

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,5 +1,4 @@
 import os
-from tkinter import SE
 
 from dotenv import load_dotenv
 
--- backend/foodgram/users/permissions.py
+++ backend/foodgram/users/permissions.py
@@ -6,30 +6,30 @@ class UserPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.action in ['list', 'create', 'retrieve'] and (
             request.user.is_authenticated or request.user.is_anonymous
-             ):
+            ):
             return True
         elif request.method in ['DELETE', 'POST', 'GET'] and (
             request.user.is_authenticated
-             ):
+            ):
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in ['DELETE', 'POST', 'GET'] and (
             request.user.is_superuser
-             ):
+            ):
             return True
         elif request.method == 'DELETE' and (
             request.user.is_authenticated
-             ):
+            ):
             subscriptions = obj.following.filter(user=request.user).exists()
             return subscriptions
         elif request.method == 'POST' and request.user.is_authenticated:
             return True
         elif request.method == 'GET' and view.action == 'list' and (
             request.user.is_authenticated
-             ):
+            ):
             return obj == request.user
         elif request.method == 'GET' and view.action == 'retrieve' and (
             request.user.is_authenticated or request.user.is_anonymous
-             ):
+            ):
             return True
