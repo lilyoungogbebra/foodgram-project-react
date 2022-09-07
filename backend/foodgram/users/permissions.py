804e8891460c977d6ac06de3e45610a8bc95e420
Ян Мелехин <vladmakaroff.makarov@yandex.ru>
2022-09-07 13:21:14 +0500

    h

--- backend/foodgram/users/permissions.py
+++ backend/foodgram/users/permissions.py
@@ -16,20 +16,20 @@ class UserPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ['DELETE', 'POST', 'GET'] and (
             request.user.is_superuser
-             ):
+        ):
             return True
         elif request.method == 'DELETE' and (
             request.user.is_authenticated
-             ):
+        ):
             subscriptions = obj.following.filter(user=request.user).exists()
             return subscriptions
         elif request.method == 'POST' and request.user.is_authenticated:
             return True
         elif request.method == 'GET' and view.action == 'list' and (
             request.user.is_authenticated
-             ):
+        ):
             return obj == request.user
         elif request.method == 'GET' and view.action == 'retrieve' and (
             request.user.is_authenticated or request.user.is_anonymous
-             ):
+        ):
             return True
